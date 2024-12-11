b31c1c213c02e4df46bd23185036969450f31273
wilderif <woojuro3@gmail.com>
2024-12-12 02:32:57 +0900

    feat: Add static images with responsive display using Next.js Image
    - Added static images using Next.js Image component in page.tsx
    - Configured responsive display with Tailwind CSS md:block and md:hidden

--- nextjs-dashboard/app/page.tsx
+++ nextjs-dashboard/app/page.tsx
@@ -2,6 +2,8 @@ import AcmeLogo from "@/app/ui/acme-logo";
 import { ArrowRightIcon } from "@heroicons/react/24/outline";
 import Link from "next/link";
 import { lusitana } from "@/app/ui/fonts";
+import Image from "next/image";
+
 export default function Page() {
   return (
     <main className="flex min-h-screen flex-col p-6">
@@ -28,7 +30,20 @@ export default function Page() {
           </Link>
         </div>
         <div className="flex items-center justify-center p-6 md:w-3/5 md:px-28 md:py-12">
-          {/* Add Hero Images Here */}
+          <Image
+            src="/hero-desktop.png"
+            width={1000}
+            height={760}
+            className="hidden md:block"
+            alt="Screenshots of the dashboard and invoices pages"
+          />
+          <Image
+            src="/hero-mobile.png"
+            width={560}
+            height={620}
+            className="block md:hidden"
+            alt="Screenshots of the dashboard and invoices pages"
+          />
         </div>
       </div>
     </main>
