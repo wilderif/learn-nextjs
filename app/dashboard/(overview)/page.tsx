07395a7f7f656ab8a8221e18950fb62062b4c0a3
wilderif <woojuro3@gmail.com>
2024-12-14 17:47:17 +0900

    feat: Add Partial Prerendering setup with PPR options
    - Added 'experimental.ppr' option in next.config.js for enabling Partial Prerendering
    - Configured 'experimental_ppr = true' in the dashboard layout file
    (commented due to version limitation)

--- app/dashboard/(overview)/page.tsx
+++ app/dashboard/(overview)/page.tsx
@@ -2,7 +2,6 @@ import CardWrapper from "@/app/ui/dashboard/cards";
 import RevenueChart from "@/app/ui/dashboard/revenue-chart";
 import LatestInvoices from "@/app/ui/dashboard/latest-invoices";
 import { lusitana } from "@/app/ui/fonts";
-import { fetchCardData } from "@/app/lib/data";
 import { Suspense } from "react";
 import {
   CardsSkeleton,
@@ -13,12 +12,12 @@ import {
 export default async function Page() {
   // const revenue = await fetchRevenue();
   // const latestInvoices = await fetchLatestInvoices();
-  const {
-    totalPaidInvoices,
-    totalPendingInvoices,
-    numberOfInvoices,
-    numberOfCustomers,
-  } = await fetchCardData();
+  // const {
+  //   totalPaidInvoices,
+  //   totalPendingInvoices,
+  //   numberOfInvoices,
+  //   numberOfCustomers,
+  // } = await fetchCardData();
   // const [
   //   latestInvoices,
   //   {
