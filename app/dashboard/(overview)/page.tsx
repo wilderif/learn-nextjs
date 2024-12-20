f0d1a494b27a750bdd507e784a42b253e8bee3f7
wilderif <woojuro3@gmail.com>
2024-12-21 02:57:27 +0900

    feat: Add metadata for SEO and social sharing
    - Added 'favicon' for browser tab icon
    - Included 'opengraph-image' for social media sharing previews
    - Defined 'title' with template and default values for consistent page titles
    - Added 'description' to enhance search engine visibility and user understanding
    - Configured 'metadataBase' for absolute URL generation in metadata

--- app/dashboard/(overview)/page.tsx
+++ app/dashboard/(overview)/page.tsx
@@ -8,6 +8,11 @@ import {
   RevenueChartSkeleton,
   LatestInvoicesSkeleton,
 } from "@/app/ui/skeletons";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Dashboard",
+};
 
 export default async function Page() {
   // const revenue = await fetchRevenue();
