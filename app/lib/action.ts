8bbd3bdd327d605f5e14ec75fd92645197816474
wilderif <woojuro3@gmail.com>
2024-12-21 01:31:30 +0900

    feat: Add authenticate server action function for user sign-in
    - Implemented 'authenticate' function to handle user sign-in using the 'credentials' provider
    - Included specific messaging for invalid credentials and generic errors

--- app/lib/action.ts
+++ app/lib/action.ts
@@ -4,6 +4,8 @@ import { z } from "zod";
 import { sql } from "@vercel/postgres";
 import { revalidatePath } from "next/cache";
 import { redirect } from "next/navigation";
+import { signIn } from "@/auth";
+import { AuthError } from "next-auth";
 
 // Validate the formData before saving it to a database
 const FormSchema = z.object({
@@ -120,5 +122,24 @@ export async function deleteInvoice(id: string) {
     // return {
     //   message: "Database Error: Failed to Delete Invoice.",
     // };
+  }
+}
+
+export async function authenticate(
+  prevState: string | undefined,
+  formData: FormData
+) {
+  try {
+    await signIn("credentials", formData);
+  } catch (error) {
+    if (error instanceof AuthError) {
+      switch (error.type) {
+        case "CredentialsSignin":
+          return "Invalid credentials.";
+        default:
+          return "Something went wrong.";
+      }
+    }
+    throw error;
   }
 }
